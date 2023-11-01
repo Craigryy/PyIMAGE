d5b8086358a800a777c78c3884910283391c67d9
Craigryy <harriajames97@gmail.com>
2023-11-01 15:30:10 +0100

    added a edit , share and delete button

--- pyIMAGE/processer/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import UploadedImage
-
-class UploadedImageForm(forms.ModelForm):
-    class Meta:
-        model = UploadedImage
-        fields = ['image']  
